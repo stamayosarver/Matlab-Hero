ca82e93f9494ac1ed7a20697295328d7c2353ece
Samuel Tamayo-Sarver <samuel.tamayo-sarver@vanderbilt.edu>
2025-02-04 17:32:30 -0600

    Create one.m

---
+++ pages/Practice-Problems/Corner-Case/one.m
@@ -0,0 +1,20 @@
+Create a function, function(one) that runs a b c and d of the following in MATLAB 
+
+For questions 1a, 1b, 3b, 3c, and 3d nd the answers using a direct evaluation of the
+appropriate probability mass function or cumulative distribution function, and then
+verify your answer using MATLAB’s built-in pdf, cdf , or icdf functions.
+Also, note that some problems in this assignment can be practically solved using a
+binomial random variable OR a Poisson random variable. Give some thought to why
+that is and be prepared to discuss in class on Feb 5.
+1. Newborns were screened for human immunodeciency virus (HIV) in 5
+Massachusetts hospitals. At one representative hospital, 5110 tests were
+performed and 44 tests were positive. If you assume the rate of positive HIV tests
+remains constant, for the next 100 tests,
+a) what is the probability of 0 positive tests?
+b) what is the probability of more than 3 positive tests?
+c) what is the expected value of the number of positive tests?
+d) If the probability of an HIV infection is the same at 10,000 different hospitals,
+what do you expect for the variance in the number of positive tests (from the
+next 100 tests) from across these 10,000 (or any really large number) hospitals? 
+
+
